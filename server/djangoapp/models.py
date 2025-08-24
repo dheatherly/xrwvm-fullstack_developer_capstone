2c0b99716423fe01297a6f00a2936b934ab82dfe
dheatherly <dheatherly@users.noreply.github.com>
2025-08-24 11:37:18 -0400

    Add files via upload

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,11 +7,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    fuel = [
-        ('GAS', 'gas'),
-        ('HYBRID', 'hybrid'),
-        ('EV', 'ev'),
-    ]
 
     def __str__(self):
         return self.name  # Return the name as the string representation
